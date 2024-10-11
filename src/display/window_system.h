2beb12d221deadb80490d8e5af1895da610113c0
Viet Dang <dangvd@users.noreply.github.com>
2024-10-11 22:54:51 +0100

    Corrects activeWindowChanged signal signature #65

--- src/display/window_system.h
+++ src/display/window_system.h
@@ -84,7 +84,7 @@ class WindowSystem : public QObject {
   void windowLeftCurrentDesktop(std::string_view);
   void windowGeometryChanged(const WindowInfo*);
   void windowStateChanged(const WindowInfo*);
-  void activeWindowChanged(std::string);
+  void activeWindowChanged(std::string_view);
 
   void desktopNameChanged(std::string_view desktopId, std::string_view desktopName);
 
