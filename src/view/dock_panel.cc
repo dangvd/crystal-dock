33d10fc77d443de06623c4938533f5b4b5908a8a
Viet Dang <dangvd@users.noreply.github.com>
2024-10-19 14:24:08 +0100

    Sets version to 2.7 alpha

--- src/view/dock_panel.cc
+++ src/view/dock_panel.cc
@@ -66,7 +66,7 @@ DockPanel::DockPanel(MultiDockView* parent, MultiDockModel* model, int dockId)
       showClock_(false),
       showBorder_(true),
       aboutDialog_(QMessageBox::Information, "About Crystal Dock",
-                   QString("<h3>Crystal Dock 2.6</h3>")
+                   QString("<h3>Crystal Dock 2.7 alpha</h3>")
                    + "<p>Copyright (C) 2024 Viet Dang (dangvd@gmail.com)"
                    + "<p><a href=\"https://github.com/dangvd/crystal-dock\">https://github.com/dangvd/crystal-dock</a>"
                    + "<p>License: GPLv3",
