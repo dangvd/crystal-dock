95668c4bfd6f8c7d527d88dfd285f435fa23d32f
Viet Dang <dangvd@users.noreply.github.com>
2025-05-18 16:32:10 +0100

    Sets version to 2.14 RC1

--- src/view/dock_panel.cc
+++ src/view/dock_panel.cc
@@ -65,7 +65,7 @@ DockPanel::DockPanel(MultiDockView* parent, MultiDockModel* model, int dockId)
       showPager_(false),
       showClock_(false),
       aboutDialog_(QMessageBox::Information, "About Crystal Dock",
-                   QString("<h3>Crystal Dock 2.14 alpha</h3>")
+                   QString("<h3>Crystal Dock 2.14 RC1</h3>")
                    + "<p>Copyright (C) 2025 Viet Dang (dangvd@gmail.com)"
                    + "<p><a href=\"https://github.com/dangvd/crystal-dock\">https://github.com/dangvd/crystal-dock</a>"
                    + "<p>License: GPLv3",
