2f091a3a769dfc93423c4804ff9dfa8fcbdabe5c
Viet Dang <dangvd@users.noreply.github.com>
2025-10-20 17:09:18 +0100

    Sets version to 2.15 RC11

--- src/view/dock_panel.cc
+++ src/view/dock_panel.cc
@@ -65,7 +65,7 @@ namespace ranges = std::ranges;
 
 namespace crystaldock {
 
-/*static*/ constexpr char DockPanel::kVersion[] = "2.15 RC10";
+/*static*/ constexpr char DockPanel::kVersion[] = "2.15 RC11";
 
 DockPanel::DockPanel(MultiDockView* parent, MultiDockModel* model, int dockId)
     : QWidget(),
