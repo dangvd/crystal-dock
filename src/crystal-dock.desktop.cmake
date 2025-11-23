da266dd0f9a2797f404ec02588e80aae46a26d8a
Viet Dang <dangvd@users.noreply.github.com>
2025-11-23 20:52:46 +0000

    Adds keywords for desktop file #213

--- src/crystal-dock.desktop.cmake
+++ src/crystal-dock.desktop.cmake
@@ -6,4 +6,5 @@ Icon=user-desktop
 Exec=@CMAKE_INSTALL_PREFIX@/bin/crystal-dock
 Terminal=false
 Categories=Utility;
+Keywords=Dock;Launcher;Taskbar;Desktop;Panel;
 X-KDE-Wayland-Interfaces=org_kde_plasma_window_management
